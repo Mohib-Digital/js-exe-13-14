cadb145e87ecf59b1c3f538be296b8d73c0fdb7c
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-08 16:38:27 -0700

    add codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,132 @@
 
-var gpa = +prompt("Please! Enter your marks?")
+// Chapter 13  (Testing Sets of Conditions)
 
-if (gpa >= 90) {
-    console.log("You got A+ and Your Grade Point is 4.0")
+// Grade Points System
+
+// var gpa = +prompt("Please! Enter your marks?")
+
+// if (gpa >= 90) {
+//     console.log("You got A+ and Your Grade Point is 4.0")
+// }
+
+// else if (gpa >= 85 && gpa <= 89 ) {
+//     console.log("You got A and Your Grade Point is 4.0")
+// }
+
+// else if (gpa >= 80 && gpa <= 84) {
+//     console.log("You got A- and Your Grade Point is 3.8")
+// }
+
+// else if (gpa >= 75 && gpa <= 79) {
+//     console.log("You got B+ and Your Grade Point is 3.4")
+// }
+
+// else if (gpa >= 71 && gpa <= 74) {
+//     console.log("You got B and Your Grade Point is 3.0")
+// }
+
+// else if (gpa >= 68 && gpa <= 70) {
+//     console.log("You got B- and Your Grade Point is 2.8")
+// }
+
+// else if (gpa >= 64 && gpa <= 67) {
+//     console.log("You got C+ and Your Grade Point is 2.4")
+// }
+
+// else if (gpa >= 61 && gpa <= 63) {
+//     console.log("You got C and Your Grade Point is 2.0")
+// }
+
+// else if (gpa >= 57 && gpa <= 60) {
+//     console.log("You got C- and Your Grade Point is 1.8")
+// }
+
+// else if (gpa >= 53 && gpa <= 56) {
+//     console.log("You got D+ and Your Grade Point is 1.4")
+// }
+
+// else if (gpa >= 50 && gpa <= 52) {
+//     console.log("You got D and Your Grade Point is 1.0")
+// }
+
+// else {
+//     console.log("Sorry you are failed!")
+// }
+
+
+
+// let firstNumber = +prompt("Enter your first number:")
+// let secondNumber = +prompt("Enter your second number:")
+// let thirdNumber = +prompt("Enter your third number:")
+
+// if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+//     console.log(`First numbr that is ${firstNumber} is the largest number`)
+// }
+
+// else if (secondNumber > thirdNumber) {
+//     console.log(`Second number that is ${secondNumber} is the largest number`)
+// }
+
+// else {
+//     console.log(`Third number that is ${thirdNumber}, is the largest number`)
+// }
+
+// ===================================================================================
+
+// Chapter 14  (if statements nested)
+
+// nested if statement means that aik k andar dusra if statment ko use krne ko nested if kaha jata hai
+
+// user Authentication
+
+// let username = prompt("Please! Enter your username?")
+// let password;
+
+// if (username === "admin") {
+//     password = prompt("Please! Enter your password?")
+//     if (password === "guest") {
+//         console.log("Login Successfull. You are welcome!")
+//     }
+//     else {
+//         console.log("Incorrect password! try again!")
+//     }
+// } 
+// else {
+//     console.log("User not found, Please enter a correct username.")
+// }
+// =========================================
+
+//For Government Job
+
+var age = +prompt("Enter your age!")
+if(age >= 18 && age <= 25){
+var gender = prompt("Enter Gender")
+if(gender === "Female"){
+console.log("You are eligible");
+}else{
+console.log("This is only for female")
+}
 }
 
-else if (gpa >= 85 && gpa <= 89 ) {
-    console.log("You got A and Your Grade Point is 4.0")
+else{
+console.log("You are not Eligible")
 }
 
-else if (gpa >= 80 && gpa <= 84) {
-    console.log("You got A- and Your Grade Point is 3.8")
-}
 
-else if (gpa >= 75 && gpa <= 79) {
-    console.log("You got B+ and Your Grade Point is 3.4")
-}
 
-else if (gpa >= 71 && gpa <= 74) {
-    console.log("You got B and Your Grade Point is 3.0")
-}
 
-else if (gpa >= 68 && gpa <= 70) {
-    console.log("You got B- and Your Grade Point is 2.8")
-}
 
-else if (gpa >= 64 && gpa <= 67) {
-    console.log("You got C+ and Your Grade Point is 2.4")
-}
 
-else if (gpa >= 61 && gpa <= 63) {
-    console.log("You got C and Your Grade Point is 2.0")
-}
 
-else if (gpa >= 57 && gpa <= 60) {
-    console.log("You got C- and Your Grade Point is 1.8")
-}
 
-else if (gpa >= 53 && gpa <= 56) {
-    console.log("You got D+ and Your Grade Point is 1.4")
-}
 
-else if (gpa >= 50 && gpa <= 52) {
-    console.log("You got D and Your Grade Point is 1.0")
-}
 
-else {
-    console.log("Sorry you are failed!")
-}
+
+
+
+
+
+
 
 
 
