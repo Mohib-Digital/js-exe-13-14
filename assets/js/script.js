f5e17026c8cbbfc524545fb90301ab757626a447
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-08 13:05:04 -0700

    add a program about grade points

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+
+var gpa = +prompt("Please! Enter your marks?")
+
+if (gpa >= 90) {
+    console.log("You got A+ and Your Grade Point is 4.0")
+}
+
+else if (gpa >= 85 && gpa <= 89 ) {
+    console.log("You got A and Your Grade Point is 4.0")
+}
+
+else if (gpa >= 80 && gpa <= 84) {
+    console.log("You got A- and Your Grade Point is 3.8")
+}
+
+else if (gpa >= 75 && gpa <= 79) {
+    console.log("You got B+ and Your Grade Point is 3.4")
+}
+
+else if (gpa >= 71 && gpa <= 74) {
+    console.log("You got B and Your Grade Point is 3.0")
+}
+
+else if (gpa >= 68 && gpa <= 70) {
+    console.log("You got B- and Your Grade Point is 2.8")
+}
+
+else if (gpa >= 64 && gpa <= 67) {
+    console.log("You got C+ and Your Grade Point is 2.4")
+}
+
+else if (gpa >= 61 && gpa <= 63) {
+    console.log("You got C and Your Grade Point is 2.0")
+}
+
+else if (gpa >= 57 && gpa <= 60) {
+    console.log("You got C- and Your Grade Point is 1.8")
+}
+
+else if (gpa >= 53 && gpa <= 56) {
+    console.log("You got D+ and Your Grade Point is 1.4")
+}
+
+else if (gpa >= 50 && gpa <= 52) {
+    console.log("You got D and Your Grade Point is 1.0")
+}
+
+else {
+    console.log("Sorry you are failed!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
